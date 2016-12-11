d680c970ee1660249ea2cadab82f4ea96bf1fc44
dspera00 <dspera00@fesb.hr>
2016-12-12 00:24:07 +0100

    Domaci

---
+++ domaci-angular-uvod/script.js
@@ -0,0 +1,15 @@
+var appModule = angular.module("hwapp", []);
+
+appModule.controller("initialController", function($scope) {
+	$scope.prvi = "";
+	$scope.drugi = "";
+
+	$scope.clean = function() {
+		$scope.prvi = "";
+		$scope.drugi = "";
+	};
+
+	$scope.hide = function() {
+		return ($scope.prvi + $scope.drugi) < 100;
+	}
+});
